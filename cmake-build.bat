9156e42cf947ce3cc600518fc229061f2b58180f
4911 <85006540+w4x9c11@users.noreply.github.com>
2025-09-30 10:32:37 +0800

    Create cmake-build.bat

---
+++ cmake-build.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal
+
+set CONFIG=%1
+
+if "%CONFIG%"=="" (
+    echo select: build.bat [Debug^|Release]
+    exit /b 1
+)
+
+cmake -S . -B build/%CONFIG% -G "Ninja" -DCMAKE_BUILD_TYPE=%CONFIG%
+cmake --build build/%CONFIG%
+
+echo ===============================
+echo Build %CONFIG% Success.
+echo ===============================
