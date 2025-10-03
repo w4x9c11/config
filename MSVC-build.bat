2888d8c695d0af00659af0ef4360156c40e6e236
4911 <85006540+w4x9c11@users.noreply.github.com>
2025-10-03 18:23:54 +0800

    build.bat

--- MSVC-build.bat
+++ MSVC-build.bat
@@ -4,13 +4,15 @@ setlocal
 set CONFIG=%1
 
 if "%CONFIG%"=="" (
-    echo select: build.bat [Debug^|Release]
+    echo Usage: build.bat [Debug^|Release]
     exit /b 1
 )
 
-cmake -S . -B build/%CONFIG% -G "Ninja" -DCMAKE_BUILD_TYPE=%CONFIG%
-cmake --build build/%CONFIG%
+cmake -S . -B build -G "Ninja" -DCMAKE_BUILD_TYPE=%CONFIG%
 
-echo =============================== 
+REM 执行构建
+cmake --build build --config %CONFIG%
+
+echo ===============================
 echo Build %CONFIG% Success.
 echo ===============================
